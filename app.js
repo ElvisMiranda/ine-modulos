af43de0283c9d6537f63a1057f56326ec8532cb0
Elvis Miranda <elvis.miranda@live.com>
2024-04-17 23:43:55 -0400

    Add CRUD operations for Class model

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 mongoose.connect(process.env.MONGODB_URI);
 
 const courseService = require('./services/courseService');
+const classService = require('./services/classService');
 
 const port = process.env.PORT || 3000;
 
@@ -16,18 +17,34 @@ app.use(cors({ optionsSuccessStatus: 200 }));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(bodyParser.json())
+
 app.get('/api/courses', async (req, res) => {
     res.send(await courseService.getAll());
 });
 
 app.get('/api/courses/:courseId/classes', async (req, res) => {
-    
-}).post('/api/courses/:courseId/classes', async (req, res) => {
+    const courseId = req.params.courseId;
+    res.send(await classService.getByCourse(courseId));
+});
 
-}).put('/api/courses/:courseId/classes/:classId', async (req, res) => {
+app.get('/api/classes/:classId', async (req, res) => {
+    const classId = req.params.classId;
+    res.send(await classService.get(classId));
+}).post('/api/classes', async (req, res) => {
+    const classEntry = req.body;
 
-}).delete('/api/courses/:courseId/classes/:classId', async (req, res) => {
+    res.send(await classService.add(classEntry));
+}).put('/api/classes/:classId', async (req, res) => {
+    const classId = req.params.classId;
+    const classEntry = req.body;
 
+    res.send(await classService.update(classId, classEntry));
+
+}).delete('/api/classes/:classId', async (req, res) => {
+    const classId = req.params.classId;
+    await classService.delete(classId);
+    res.send({ status: 'Ok'});
 });
 
 var listener = app.listen(port, _ => {
--- services/classService.js
+++ services/classService.js
@@ -5,9 +5,29 @@ module.exports = {
         return await Class.find({ courseId: courseId });
     },
 
-    addClass: async (classEntry) => {
+    add: async (classEntry) => {
         var $class = new Class(classEntry);
 
-        await $class.save();
+        return await $class.save();
+    },
+
+    update: async (classId, classEntry) => {
+        var $class = await Class.findById(classId);
+
+        $class.name = classEntry.name;
+        $class.date = classEntry.date;
+        $class.startTime = classEntry.startTime;
+        $class.endTime = classEntry.endTime;
+        $class.mode = classEntry.mode;
+
+        return await $class.save();
+    },
+
+    get: async (classId) => {
+        return await Class.findById(classId);
+    },
+
+    delete: async (classId) => {
+        await Class.deleteOne({ _id: classId });
     }
 };
