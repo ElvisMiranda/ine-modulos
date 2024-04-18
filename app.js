ecbd163cf1f1505807b980137aeea084c37296c4
Elvis Miranda <elvis.miranda@live.com>
2024-04-17 22:17:58 -0400

    Add initial endpoints for Class CRUD operations

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ app.get('/api/courses', async (req, res) => {
     res.send(await courseService.getAll());
 });
 
+app.get('/api/courses/:courseId/classes', async (req, res) => {
+    
+}).post('/api/courses/:courseId/classes', async (req, res) => {
+
+}).put('/api/courses/:courseId/classes/:classId', async (req, res) => {
+
+}).delete('/api/courses/:courseId/classes/:classId', async (req, res) => {
+
+});
+
 var listener = app.listen(port, _ => {
     console.log(`Listening on port ${port}`);
 });
